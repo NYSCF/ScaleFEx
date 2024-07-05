effcbb531e3eb1ed6cadc3799cea33e3e618cf10
Gabcomolet <gcomolet@nyscf.org>
2024-07-05 10:41:16 +0200

    Merged params + -all plates available + plate delimiters used

--- AWS_scalefex_extraction.py
+++ AWS_scalefex_extraction.py
@@ -20,50 +20,50 @@ class Screen_Compute: #come up with a better name
     Class representing the computation of screen data.
 
     Methods:
-        __init__(yaml_path='AWS_params.yaml'): 
-            Initializes the Screen_Compute object with AWS_params from a YAML file.
+        __init__(yaml_path='parameters.yaml'): 
+            Initializes the Screen_Compute object with parameters from a YAML file.
     """
-    def __init__(self, yaml_path='AWS_params.yaml'):
+    def __init__(self, yaml_path='parameters.yaml'):
         """
-        Initializes the Screen_Compute object with AWS_params from a YAML file.
+        Initializes the Screen_Compute object with parameters from a YAML file.
 
         Args:
-            yaml_path (str): Path to the YAML file containing AWS_params. Default is 'AWS_params.yaml'.
+            yaml_path (str): Path to the YAML file containing parameters. Default is 'parameters.yaml'.
         """
 
         # Read the yaml file
         with open(yaml_path, 'rb') as f:
-            self.AWS_params = yaml.load(f.read(), Loader=yaml.CLoader)
-        self.files = dq.query_data(self.AWS_params['fname_pattern'],plate_identifier = self.AWS_params['plate_identifier'],
-                              exts=self.AWS_params['file_extensions'],experiment_name = self.AWS_params['experiment_name'],
-                              plates=self.AWS_params['plates'], s3_bucket = self.AWS_params['s3_bucket'])
+            self.parameters = yaml.load(f.read(), Loader=yaml.CLoader)
+        self.files,plates = dq.query_data(self.parameters['pattern'],plate_identifiers = self.parameters['plate_identifiers'],exp_folder = self.parameters['exp_folder'], 
+                              exts=self.parameters['exts'],experiment_name = self.parameters['experiment_name'],
+                              plates=self.parameters['plates'], s3_bucket = self.parameters['s3_bucket'])
 
-        print(self.files.head())
+        print(self.files)
         
         self.vec_dir = 'scalefex'
         if not os.path.exists(self.vec_dir):
             os.makedirs(self.vec_dir)  
-        ffc_file = os.path.join(self.vec_dir,self.AWS_params['experiment_name'] + '_FFC.p')
+        ffc_file = os.path.join(self.vec_dir,self.parameters['experiment_name'] + '_FFC.p')
         self.flat_field_correction = {}
-        if self.AWS_params['FFC'] is True and os.path.exists(ffc_file):
+        if self.parameters['FFC'] is True and os.path.exists(ffc_file):
                 print(ffc_file + ' Found generating FFC now')
         else:
-            for channel in self.AWS_params['channel']:
+            for channel in self.parameters['channel']:
                 self.flat_field_correction[channel] = 1
 
-        self.plate = self.AWS_params['plates'][0]
+        self.plate = plates[0]
         
-        if self.AWS_params['QC']==True:
-            self.csv_fileQC = os.path.join(self.vec_dir,'QC_'+self.AWS_params['experiment_name']+'_'+str(self.plate)+'_'
-                    +str(self.AWS_params['subset_index'])+'.csv') 
+        if self.parameters['QC']==True:
+            self.csv_fileQC = os.path.join(self.vec_dir,'QC_'+self.parameters['experiment_name']+'_'+str(self.plate)+'_'
+                    +str(self.parameters['subset_index'])+'.csv') 
         self.start_computation(self.plate, self.files)
 
     def compute_vector(self,well):
         ''' Function that imports the images and extracts the location of cells'''
 
         print(well, self.plate, datetime.now())
-        csv_file = os.path.join(self.vec_dir,'SF_'+self.AWS_params['experiment_name']+'_'+self.plate+'_'
-                            +str(self.AWS_params['subset_index'])+'_01'+'.csv')
+        csv_file = os.path.join(self.vec_dir,'SF_'+self.parameters['experiment_name']+'_'+self.plate+'_'
+                            +str(self.parameters['subset_index'])+'_01'+'.csv')
         
         sites = np.unique(self.task_files.site)
         sites.sort()
@@ -71,10 +71,10 @@ class Screen_Compute: #come up with a better name
         for site in sites:
             print(site, well, self.plate, datetime.now())
             np_images, original_images = dq.load_and_preprocess(self.task_files,
-                                self.AWS_params['channel'],well,site,self.AWS_params['zstack'],
-                                self.AWS_params['image_size'],self.flat_field_correction,
-                                self.AWS_params['downsampling'],return_original=self.AWS_params['QC'],
-                                s3_bucket = self.AWS_params['s3_bucket'])
+                                self.parameters['channel'],well,site,self.parameters['zstack'],
+                                self.parameters['image_size'],self.flat_field_correction,
+                                self.parameters['downsampling'],return_original=self.parameters['QC'],
+                                s3_bucket = self.parameters['s3_bucket'])
             
             try:
                 original_images.shape
@@ -82,7 +82,7 @@ class Screen_Compute: #come up with a better name
                 print('Images corrupted')
             if np_images is not None:
                 
-                if self.AWS_params['csv_coordinates']=='' or self.AWS_params['csv_coordinates'] is None:
+                if self.parameters['csv_coordinates']=='' or self.parameters['csv_coordinates'] is None:
                     center_of_mass=self.segment_crop_images(np_images[0,:,:,0])
                     center_of_mass=[list(row) + [n] for n,row in enumerate(center_of_mass)]
                     
@@ -92,22 +92,22 @@ class Screen_Compute: #come up with a better name
                     center_of_mass=np.asarray(locations[['coordX','coordY','cell_id']])
                     
                 live_cells=len(center_of_mass)  
-                if self.AWS_params['QC']==True:
+                if self.parameters['QC']==True:
                     indQC=0
                     QC_vector,indQC = Quality_control_HCI.compute_global_values.calculateQC(len(center_of_mass),live_cells,
-                                        'scalefex',original_images,well,self.plate,site,self.AWS_params['channel'],
-                                        indQC,self.AWS_params['neurite_tracing'])
+                                        'scalefex',original_images,well,self.plate,site,self.parameters['channel'],
+                                        indQC,self.parameters['neurite_tracing'])
                     self.csv_fileQC = dq.save_qc_file(QC_vector,self.csv_fileQC)
             
                 for x,y,n in center_of_mass:
-                    crop=np_images[:,int(float(x)-self.AWS_params['ROI']):int(float(x)+self.AWS_params['ROI']),
-                                        int(float(y)-self.AWS_params['ROI']):int(float(y)+self.AWS_params['ROI']),:]
-                    if crop.shape != (len(self.AWS_params['channel']),self.AWS_params['ROI']*2,self.AWS_params['ROI']*2,1):
+                    crop=np_images[:,int(float(x)-self.parameters['ROI']):int(float(x)+self.parameters['ROI']),
+                                        int(float(y)-self.parameters['ROI']):int(float(y)+self.parameters['ROI']),:]
+                    if crop.shape != (len(self.parameters['channel']),self.parameters['ROI']*2,self.parameters['ROI']*2,1):
                         continue
                     else:
                         ind=0
                         vector=pd.DataFrame(np.asarray([self.plate,well,site,x,y,n]).reshape(1,6),columns=['plate','well','site','coordX','coordY','cell_id'],index=[ind])
-                        if self.AWS_params['csv_coordinates']=='' or self.AWS_params['csv_coordinates'] is None:
+                        if self.parameters['csv_coordinates']=='' or self.parameters['csv_coordinates'] is None:
                             tree = KDTree([row[:2] for row in center_of_mass])
                             # Query the nearest distance and the index of the nearest point
                             distance, _ = tree.query([x,y], k=2)    
@@ -118,18 +118,18 @@ class Screen_Compute: #come up with a better name
                         try:
                             scalefex = ScaleFEx_from_crop.compute_ScaleFEx.ScaleFEx(
                                 crop,
-                                channel=self.AWS_params['channel'],
-                                mito_ch=self.AWS_params['Mito_channel'],
-                                rna_ch=self.AWS_params['RNA_channel'],
-                                downsampling=self.AWS_params['downsampling'],
-                                roi=int(self.AWS_params['ROI'])
+                                channel=self.parameters['channel'],
+                                mito_ch=self.parameters['Mito_channel'],
+                                rna_ch=self.parameters['RNA_channel'],
+                                downsampling=self.parameters['downsampling'],
+                                roi=int(self.parameters['ROI'])
                             ).single_cell_vector
 
                             if isinstance(scalefex, pd.DataFrame):
                                 vector = pd.concat([vector, scalefex], axis=1)
-                                csv_file = dq.save_csv_file(vector, csv_file, self.AWS_params['max_file_size'],
-                                        self.AWS_params['s3_bucket'], self.AWS_params['experiment_name'],
-                                        self.plate, self.AWS_params['subset_index'])
+                                csv_file = dq.save_csv_file(vector, csv_file, self.parameters['max_file_size'],
+                                        self.parameters['s3_bucket'], self.parameters['experiment_name'],
+                                        self.plate, self.parameters['subset_index'])
                                                 
                         except Exception as e:
                             print("An error occurred during ScaleFEx computation:", e)
@@ -138,34 +138,35 @@ class Screen_Compute: #come up with a better name
             
     def start_computation(self,plate,files):
         
-        self.task_files = dq.filter_task_files(files,self.AWS_params['subset_index'], self.AWS_params['nb_subsets']) 
+        self.task_files = dq.filter_task_files(files,self.parameters['subset_index'], self.parameters['nb_subsets']) 
 
-        if self.AWS_params['csv_coordinates'] is not None and os.path.exists(self.AWS_params['csv_coordinates']):
-            self.locations=pd.read_csv(self.AWS_params['csv_coordinates'])
+        if self.parameters['csv_coordinates'] is not None and os.path.exists(self.parameters['csv_coordinates']):
+            self.locations=pd.read_csv(self.parameters['csv_coordinates'])
             self.locations=self.locations.loc[self.locations.plate.astype(str)==str(plate)]
             self.locations = dq.filter_coord(self.locations, self.task_files)
             
             wells=np.unique(self.locations.well)
+       
         wells=np.unique(self.task_files.well)
 
         function = self.compute_vector
-        parallelize_computation.parallelize(wells,function,self.AWS_params['n_of_workers'],mode = 'dev')
+        parallelize_computation.parallelize(wells,function,self.parameters['n_of_workers'],mode = 'dev')
             
         print('All processes have completed their tasks.')
         
-        dq.push_all_files(self.AWS_params['s3_bucket'],self.AWS_params['experiment_name'],
-                                                self.AWS_params['plates'],self.AWS_params['subset_index'],self.vec_dir)
+        dq.push_all_files(self.parameters['s3_bucket'],self.parameters['experiment_name'],
+                                                plate,self.parameters['subset_index'],self.vec_dir)
         dq.terminate_current_instance()
 
     def segment_crop_images(self,img_nuc):
 
         # extraction of the location of the cells
-        nls=import_module(self.AWS_params['segmenting_function'])
+        nls=import_module(self.parameters['segmenting_function'])
         
         img_mask=nls.compute_DNA_mask(img_nuc)
         center_of_mass = nls.retrieve_coordinates(img_mask,
-                    cell_size_min=self.AWS_params['min_cell_size']*self.AWS_params['downsampling'],
-                    cell_size_max=self.AWS_params['max_cell_size']/self.AWS_params['downsampling'])
+                    cell_size_min=self.parameters['min_cell_size']*self.parameters['downsampling'],
+                    cell_size_max=self.parameters['max_cell_size']/self.parameters['downsampling'])
         try:
             center_of_mass
             print('N of cells found: ',len(center_of_mass))
--- AWS_scalefex_init.py
+++ AWS_scalefex_init.py
@@ -14,49 +14,48 @@ class Screen_Init:
     Class representing the initialisation of AWS screen.
 
     Methods:
-        __init__(yaml_path='AWS_params.yaml'): 
-            Initializes the Screen_Compute object with AWS_params from a YAML file.
+        __init__(yaml_path='parameters.yaml'): 
+            Initializes the Screen_Compute object with parameters from a YAML file.
     """
-    def __init__(self, yaml_path='AWS_params.yaml'):
+    def __init__(self, yaml_path='parameters.yaml'):
         """
-        Initializes the Screen_Compute object with AWS_params from a YAML file.
+        Initializes the Screen_Compute object with parameters from a YAML file.
 
         Args:
-            yaml_path (str): Path to the YAML file containing AWS_params. Default is 'AWS_params.yaml'.
+            yaml_path (str): Path to the YAML file containing parameters. Default is 'parameters.yaml'.
         """
         # Read the yaml file
         with open(yaml_path, 'rb') as f:
-            self.AWS_params = yaml.load(f.read(), Loader=yaml.CLoader)
-        files = dq.query_data(self.AWS_params['fname_pattern'],plate_identifier = self.AWS_params['plate_identifier'],
-                              exts=self.AWS_params['file_extensions'],experiment_name = self.AWS_params['experiment_name'],
-                              plates=self.AWS_params['plates'], s3_bucket = self.AWS_params['s3_bucket'])
+            self.parameters = yaml.load(f.read(), Loader=yaml.CLoader)
+        files,plates = dq.query_data(self.parameters['pattern'],plate_identifiers = self.parameters['plate_identifiers'], exp_folder = self.parameters['exp_folder'], 
+                              exts=self.parameters['exts'],experiment_name = self.parameters['experiment_name'],
+                              plates=self.parameters['plates'], s3_bucket = self.parameters['s3_bucket'])
         
         # Perform Flat Field Correction (FFC)
         self.flat_field_correction = {}
     
-        if self.AWS_params['FFC'] is True:
-            ffc_file = os.path.join(self.AWS_params['experiment_name'] + '_FFC.p')
+        if self.parameters['FFC'] is True:
+            ffc_file = os.path.join(self.parameters['experiment_name'] + '_FFC.p')
             if not os.path.exists(ffc_file):
                 print(ffc_file + ' Not found generating FFC now')
                 self.flat_field_correction = dq.flat_field_correction_AWS(files,ffc_file,
-                self.AWS_params['s3_bucket'],self.AWS_params['channel'],self.AWS_params['experiment_name'],
-                n_images=self.AWS_params['FFC_n_images'])
+                self.parameters['s3_bucket'],self.parameters['channel'],self.parameters['experiment_name'],n_images=self.parameters['FFC_n_images'])
             else:
                 print(ffc_file + ' Found, loading FFC')
                 self.flat_field_correction = pickle.load(open(ffc_file, "rb"))
         else:
-            for channel in self.AWS_params['channel']:
+            for channel in self.parameters['channel']:
                 self.flat_field_correction[channel] = 1
 
-        dq.upload_ffc_to_s3(self.AWS_params['s3_bucket'],'AWS_params.yaml',self.AWS_params['experiment_name'])
+        dq.upload_ffc_to_s3(self.parameters['s3_bucket'],'parameters.yaml',self.parameters['experiment_name'])
 
         if len(files) != 0:
-            instance_ids,instance_tags = dq.launch_ec2_instances(self.AWS_params['experiment_name'],self.AWS_params['region'],self.AWS_params['s3_bucket'],
-                self.AWS_params['amazon_image_id'], self.AWS_params['instance_type'], self.AWS_params['plates'], self.AWS_params['nb_subsets'],
-                self.AWS_params['csv_coordinates'],subnet_id = self.AWS_params['subnet_id'],security_group_id =self.AWS_params['security_group_id'])
+            instance_ids,instance_tags = dq.launch_ec2_instances(self.parameters['experiment_name'],self.parameters['region'],self.parameters['s3_bucket'],
+                self.parameters['amazon_image_id'], self.parameters['instance_type'], plates, self.parameters['nb_subsets'],
+                self.parameters['csv_coordinates'],subnet_id = self.parameters['subnet_id'],security_group_id =self.parameters['security_group_id'])
             scheduler = sched.scheduler(time.time, time.sleep)
             scheduler.enter(300, 1, dq.periodic_check, (scheduler, 360, dq.check_instance_metrics, (instance_ids,instance_tags,
-                                                                                                    self.AWS_params['region'],0.35,0)))
+                                                                                                    self.parameters['region'],0.35,0)))
             scheduler.run()
         
         else  :
