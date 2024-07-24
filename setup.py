62fe94e30a0a0a1a30d34ed036c7281caeed82f7
Jeff Winchell <winch.jm@gmail.com>
2024-07-24 12:25:34 -0400

    relaxed scipy requirement

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author_email='bmigliori@nyscf.org',
     url='https://github.com/NYSCF/ScaleFEx',
     install_requires=[
-        'numpy==1.26.4', 'pytest==8.2.0', 'opencv-python-headless==4.9.0.80', 'matplotlib==3.8.4', 'scipy==1.13.0', 'tifffile',
+        'numpy==1.26.4', 'pytest==8.2.0', 'opencv-python-headless==4.9.0.80', 'matplotlib==3.8.4', 'scipy=<1.14.0', 'tifffile',
         'pandas==2.2.2', 'scikit-image==0.20.0','PyYAML==6.0.1', 'utils','mahotas==1.4.15','Pillow==10.4.0','prettytable==3.10.2',
         'setuptools==68.2.2'
     ]
